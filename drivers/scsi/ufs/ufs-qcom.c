6c28907c261952fcd04a1330913c28d000cdeb16
Mimi Wu <mimiwu@google.com>
2023-06-14 22:19:46 +0100

    scsi: ufs: disable clock scaling
    Disable clock scaling to avoid costly workqueue overheads.
    
    Power test results on Blueline:
    [without this change]
      Suspend: 9.75mA
      Idle: 238.26mA
      Camera Preview: 1309.99mA
      Partial Wake Lock: 13.67mA
    [with this change - disable clock scaling]
      Suspend: 9.73mA (-0.21%)
      Idle: 215.87mA (-9.4%)
      Camera Preview: 1181.71mA (-9.79%)
      Partial Wake Lock: 13.85mA (+1.32%)
    
    Bug: 78601190
    Change-Id: I09f07619ab3e11b05149358c1d06b0d1039decf3

--- drivers/scsi/ufs/ufs-qcom.c
+++ drivers/scsi/ufs/ufs-qcom.c
@@ -2316,15 +2316,9 @@ static void ufs_qcom_set_caps(struct ufs_hba *hba)
 {
 	struct ufs_qcom_host *host = ufshcd_get_variant(hba);
 
-	if (!host->disable_lpm) {
-		hba->caps |= UFSHCD_CAP_CLK_GATING |
-		UFSHCD_CAP_HIBERN8_WITH_CLK_GATING |
-		UFSHCD_CAP_CLK_SCALING | UFSHCD_CAP_AUTO_BKOPS_SUSPEND |
-		UFSHCD_CAP_RPM_AUTOSUSPEND;
-		hba->caps |= UFSHCD_CAP_WB_EN;
-		hba->caps |= UFSHCD_CAP_AGGR_POWER_COLLAPSE;
-	}
-
+	hba->caps |= UFSHCD_CAP_CLK_GATING | UFSHCD_CAP_HIBERN8_WITH_CLK_GATING;
+	hba->caps |= UFSHCD_CAP_AUTO_BKOPS_SUSPEND;
+	hba->caps |= UFSHCD_CAP_WB_EN;
 	hba->caps |= UFSHCD_CAP_CRYPTO;
 
 	if (host->hw_ver.major >= 0x2)
